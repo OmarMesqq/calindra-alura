e5307c9617e5d844d0fa118f75bd26f67c71cfe8
Omar Mesquita <omarmesquita@eq.ufrj.br>
2023-07-27 21:50:39 -0300

    Introduz forma melhor de lidar com fcs assincronas

--- alurabooks/script.js
+++ alurabooks/script.js
@@ -1,35 +1,20 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-	.then(resposta => resposta.json())
-	.then(r => {
-		if (r.erro) {
-			// As formas de lidar com eventuais erros variam de acordo com a API utilizada 
-			// No caso do ViaCEP, CEPs inexistentes geram uma resposta com o parâmetro erro=true
-			// Por isso, o utilizamos aqui para customizar a msg de erro
+// Funções async retornam uma Promise
+async function buscaEndereco() {
+	try { 
+		// await pausa a execução da função até que a Promise seja resolvida
+		var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/');
+		var consultaCEPConvertida = await consultaCEP.json();
+		// Lançamos erro após a conversão da resposta para JSON, porque 
+		// a chave "erro" e seu valor "true" só existem aparecem nessa etapa
+		if (consultaCEPConvertida.erro) {
 			throw Error('Esse CEP não existe')
 		}
-		else { 
-			// Aqui deu tudo certo
-			console.log(r)
-		}
-	})
-	// Os demais erros relacionados com CEPs inseridos (espaço entre dígitos, 
-	// alfanuméricos, qtd de dígitos errada,...) são respondidos com um erro 
-	// HTTP 400 Bad Request, então o catch já pega essa exceção  
-	.catch(erro => console.log(erro))
-	// Esse método é executado independentemente do tipo de resposta da Promise: 
-	// Com a API retornando "resolved" ou "rejected", a mensagem do finally será mostrada
-	.finally(mensagem => console.log('Processamento concluído'));
+		console.log(consultaCEPConvertida);
+	} 
+	catch(erro) { 
+		console.log(erro);
+	}
+	
+}
 
-console.log(consultaCEP);
-
-/* 
-Nesse código, fizemos uso de arrow functions do JavaScript. Elas são 
-um jeito mais conciso de escrever funções e utilizam o operador => 
-
-Seu  contexto é sempre o contexto no qual foi declarada, ou seja, 
-ela não tem contexto próprio. 
-
-Em outras palavras, o seu .this irá retornar o contexto de uma função 
-caso tenha sido declarada aninhada dentro de outra ou o escopo global
-caso tenha sido aplicada fora de outra função
-*/
+buscaEndereco();
