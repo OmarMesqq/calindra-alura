53573bdd8d303c30795d2df4ed11867b4c40bbd7
OmarMesqq <omarmsqt@gmail.com>
2023-08-02 23:14:36 -0300

    feat: :rocket: finalizei curso de consumo de API com JS

--- alurabooks/script.js
+++ alurabooks/script.js
@@ -1,20 +1,43 @@
 // Funções async retornam uma Promise
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
+	var mensagemErro = document.getElementById('erro');
 	try { 
 		// await pausa a execução da função até que a Promise seja resolvida
-		var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/');
+		var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
 		var consultaCEPConvertida = await consultaCEP.json();
 		// Lançamos erro após a conversão da resposta para JSON, porque 
 		// a chave "erro" e seu valor "true" só existem aparecem nessa etapa
 		if (consultaCEPConvertida.erro) {
 			throw Error('Esse CEP não existe')
 		}
-		console.log(consultaCEPConvertida);
+		var cidade = document.getElementById('cidade');
+		
+		// Atribui ao valor do elemento cidade a chave "localidade" no JSON vindo da API
+		cidade.value = consultaCEPConvertida.localidade;
+
+		console.debug(consultaCEPConvertida);
+		return consultaCEPConvertida;
 	} 
 	catch(erro) { 
-		console.log(erro);
+		mensagemErro.innerHTML = `<p>CEP Inválido</p>`;
+		console.error(erro);
 	}
-	
 }
 
-buscaEndereco();
+//let mockCEPs = ['01001000','01001001'];
+//let mockDadosDosCEPs = mockCEPs.map(valores => buscaEndereco(valores));
+
+// Fazer um syntax sugar com try/catch ou async/await aqui 
+// Porém, o último método processa Promises sequencialmente 
+// enquanto .then() paralelamente, sendo mais rápido
+//Promise.all(mockDadosDosCEPs).then(respostas => console.log(respostas));
+
+var cep = document.getElementById('cep');	// id da div q o user entra com CEP 
+// Trigga a mudança do campo do CEP quando o user clica fora da caixa
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+
+
+/* Heurísticas de Nielsen aplicadas:
+ * - Prevenção de erro: autopreenchimento dos campos de rua, UF, etc 
+ * - Reconhecimento de erro: mensagens claras e próximas de onde o erro ocorreu
+ */
