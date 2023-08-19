f71fd964a97eab02345a395153f5bcd2284c98a1
OmarMesqq <omarmsqt@gmail.com>
2023-08-19 19:22:18 -0300

    refactor: :recycle: coloca sintaxe mais moderna nos testes
    e adiciona comentarios sobre cobertura

--- testes_nodejs/test/folhaDePagamento.test.js
+++ testes_nodejs/test/folhaDePagamento.test.js
@@ -1,8 +1,16 @@
-import {somaHorasExtras} from '../index';
+import { somaHorasExtras } from '../index';
 
-test('Deve retornar a soma das horas extras', () => {
-  const esperado = 2500;
-  const retornado = somaHorasExtras(2000, 500);
+describe('Testes da folha de pagamentos', () => {
+  it('Deve retornar a soma das horas extras', () => {
+    const esperado = 2500;
+    const retornado = somaHorasExtras(2000, 500);
 
-  expect(retornado).toBe(esperado);
+    expect(retornado).toBe(esperado);
+  });
 });
+
+// testes de cobertura:
+// Stmts - "statements" ou atribuições feitas
+// Branch - bifuracações no programa como laços e condicionais
+// Funcs - funções que o teste abrange
+// Lines - linhas cobertas
